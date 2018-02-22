2b950ec61ea5cf485eb148584b276780c407c11c
smach <smachlis@gmail.com>
2018-02-21 20:32:27 -0500

    Updated file for Starbucks map to use data on GitHub

--- 03_map.R
+++ 03_map.R
@@ -1,9 +1,17 @@
-starbucks <- read.csv("data/starbucks.csv") 
+
+if(!(file.exists("starbucks.csv"))){
+  download.file("https://raw.githubusercontent.com/smach/R4JournalismBook/master/data/starbucks.csv", "starbucks.csv")
+}
+
+starbucks <- read.csv("starbucks.csv") 
 if(!require("pacman")){
   install.packages("pacman")
 }
+
 pacman::p_load("leaflet") 
 leaflet() %>% 
   addTiles() %>% 
   setView(-84.3847, 33.7613, zoom = 16) %>%
   addMarkers(data = starbucks, lat = ~ Latitude, lng = ~ Longitude, popup = starbucks$Name)
+
+
