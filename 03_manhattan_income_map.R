3e965a7a5524c94b564d6cbea353b383ce0cbfd6
smach <smachlis@gmail.com>
2018-02-22 21:29:13 -0500

    Trying to get remote data to work

--- 03_manhattan_income_map.R
+++ 03_manhattan_income_map.R
@@ -1,15 +1,15 @@
 pacman::p_load("tmap")
 pacman::p_load("tmaptools")
 pacman::p_load("sf")
-download.file("https://github.com/smach/R4JournalismBook/blob/master/data/Manhattan.Rda", "Manhattan.Rda")
+download.file("https://github.com/smach/R4JournalismBook/raw/master/data/Manhattan.Rds", "Manhattan.Rda")
+
+Manhattan <- readRDS("data/Manhattan.Rds")
 
 
-
-get_household_income_in_county <- function(countydatafile ="Manhattan.Rda"){
-  load(countydatafile)
+get_household_income_in_county <- function(countydata = Manhattan){
   tmap_mode("view")
   
-  mymap <- tm_shape(countydata) +
+  mymap <- tm_shape(Manhattan) +
     tm_polygons("MedianIncome", palette="Greens", contrast=.9, id="Tract", title="Household Income") 
   return(mymap)
 }
